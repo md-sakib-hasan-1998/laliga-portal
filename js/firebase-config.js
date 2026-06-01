f9f84eca4c7332ce6ae431bc9599c33c53bc7411
md-sakib-hasan-1998 <sakibhasn85@gmail.com>
2026-06-01 18:21:18 +0600

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,7 +1,3 @@
-// ============================================================
-// STEP 1: FILL IN YOUR FIREBASE CONFIG HERE
-// Get these values from: Firebase Console > Project Settings > Your Apps > Web App
-// ============================================================
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
@@ -12,7 +8,7 @@ const firebaseConfig = {
   projectId: "laliga-app-7f290",
   storageBucket: "laliga-app-7f290.firebasestorage.app",
   messagingSenderId: "931741581003",
-  appId: "1:931741581003:web:903bcdc224372082f4654e"
+  appId: "1:931741581003:web:903bcdc224372082f4654e",
 };
 
 const app = initializeApp(firebaseConfig);
